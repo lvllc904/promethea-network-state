42c556d30616e9ea5e6555c458f96d245ed1e2a8
Joshua Wicke <lvllc@lvhllc.org>
2025-10-09 23:35:38 +0000

    This is perfect, now we can place the Promethea icon back at the top of

--- src/components/layout/main-nav.tsx
+++ src/components/layout/main-nav.tsx
@@ -36,32 +36,30 @@ export function MainNav() {
 
   return (
     <Sidebar>
-      <SidebarHeader className="flex-grow-0 justify-center">
+      <SidebarHeader className="flex-grow-0 justify-center p-2">
+        <Link href="/" prefetch={false}>
+            <SidebarMenuButton tooltip={{ children: "Home" }} className="h-auto">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="h-8 w-8 text-primary"
+                >
+                    <path d="M12 2.5a9.5 9.5 0 0 1 9.5 9.5c0 2.22-0.76 4.26-2.06 5.88L12 21.5l-7.44-3.12A9.5 9.5 0 0 1 2.5 12 9.5 9.5 0 0 1 12 2.5Z" />
+                    <path d="M12 2.5v19" />
+                </svg>
+                <span className="text-lg font-headline font-semibold">Promethea</span>
+            </SidebarMenuButton>
+        </Link>
       </SidebarHeader>
-      <SidebarContent className="p-2 justify-center">
-        <SidebarMenu>
-           <SidebarMenuItem>
-              <Link href="/" prefetch={false}>
-                <SidebarMenuButton tooltip={{ children: "Home" }}>
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        className="h-8 w-8 text-primary"
-                    >
-                        <path d="M12 2.5a9.5 9.5 0 0 1 9.5 9.5c0 2.22-0.76 4.26-2.06 5.88L12 21.5l-7.44-3.12A9.5 9.5 0 0 1 2.5 12 9.5 9.5 0 0 1 12 2.5Z" />
-                        <path d="M12 2.5v19" />
-                    </svg>
-                    <span className="text-lg font-headline font-semibold">Promethea</span>
-                </SidebarMenuButton>
-              </Link>
-            </SidebarMenuItem>
+      <SidebarContent className="p-2 justify-center flex">
+        <SidebarMenu className="justify-center">
           {navItems.map((item) => (
             <SidebarMenuItem key={item.href}>
               <Link href={item.href}>
