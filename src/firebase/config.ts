dd30e70aa811497f0497a288179ba814c71374bf
Joshua Wicke <lvllc@lvhllc.org>
2025-10-09 01:06:48 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9105849211-9ba48",
+  "appId": "1:385120524005:web:4b37a171d154f92ddd660c",
+  "apiKey": "AIzaSyAgGHbGW611F1MOWusBeiTFV0EN9QGrkHk",
+  "authDomain": "studio-9105849211-9ba48.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "385120524005"
+};
