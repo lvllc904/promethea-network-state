de66352c8cb64675c7bf42616f49861f9c7b3761
Joshua Wicke <lvllc@lvhllc.org>
2025-10-09 20:06:04 +0000

    We still seem to be getting the exact same error, please explain to me w

--- src/firebase/non-blocking-updates.tsx
+++ src/firebase/non-blocking-updates.tsx
@@ -57,7 +57,7 @@ export function setDocumentNonBlocking(docRef: DocumentReference, data: any, opt
  * Returns the Promise for the new doc ref, but typically not awaited by caller.
  */
 export function addDocumentNonBlocking(colRef: CollectionReference, data: any) {
-  const promise = addDoc(colD, data)
+  const promise = addDoc(colRef, data)
     .catch(error => {
       errorEmitter.emit(
         'permission-error',
